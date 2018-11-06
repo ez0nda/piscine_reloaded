4afcfe7fa73c99f1053a3ee25f2a130b7a941e4c
Emmanuel ZONDA <ezonda@e1r7p10.42.fr>
2018-11-06 16:54:01 +0100

    missing_three

---
+++ ft_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezonda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 11:13:16 by ezonda            #+#    #+#             */
+/*   Updated: 2018/11/06 11:36:55 by ezonda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int i;
+	int *tab;
+
+	i = 0;
+	tab = NULL;
+	if (min >= max)
+		return (NULL);
+	if (!(tab = (int*)malloc(sizeof(int) * (max - min))))
+		return (NULL);
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	return (tab);
+}
